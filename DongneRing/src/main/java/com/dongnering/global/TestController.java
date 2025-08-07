72aae47bcc916ab616283e300841bbdc66bd98d1
ttttkii913 <dbgkdud821@daum.net>
2025-08-07 20:49:23 +0900

    Chore/#1: 공통 응답 설정

--- DongneRing/src/main/java/com/dongnering/global/TestController.java
+++ DongneRing/src/main/java/com/dongnering/global/TestController.java
@@ -1,6 +1,8 @@
 package com.dongnering.global;
 
 
+import com.dongnering.common.error.SuccessCode;
+import com.dongnering.common.template.ApiResTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("test");
+    public ApiResTemplate<String> test() {
+        return ApiResTemplate.successResponse(SuccessCode.GET_SUCCESS, "Test");
     }
 }
