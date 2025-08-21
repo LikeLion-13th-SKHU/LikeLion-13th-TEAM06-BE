327d25c781852543b73e45392b3d404204361517
shtfh <syj2003826@gmail.com>
2025-08-21 21:29:51 +0900

    refactor: 프로필/관심사 분리 #20

--- DongneRing/src/main/java/com/dongnering/member/api/dto/request/MemberSaveReqDto.java
+++ DongneRing/src/main/java/com/dongnering/member/api/dto/request/MemberSaveReqDto.java
@@ -3,7 +3,7 @@ package com.dongnering.member.api.dto.request;
 import com.dongnering.interest.domain.InterestType;
 import java.util.List;
 
-//최초 개인화저장용
+//최초 개인화  저장용
 public record MemberSaveReqDto(
         Long age,
         String location,
