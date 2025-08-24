5843f0b00d21f901a6dfc3c1a013ef7af758772c
ttttkii913 <dbgkdud821@daum.net>
2025-08-24 16:31:01 +0900

    Refactor/#20: birthday info 응답에 추가

--- DongneRing/src/main/java/com/dongnering/member/api/dto/response/MemberInfoResDto.java
+++ DongneRing/src/main/java/com/dongnering/member/api/dto/response/MemberInfoResDto.java
@@ -2,6 +2,7 @@ package com.dongnering.member.api.dto.response;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Builder
@@ -12,11 +13,12 @@ public record MemberInfoResDto(
         String location,
         String memberPictureUrl,
         List<String> interests,
-        boolean profileCompleted
+        boolean profileCompleted,
+        LocalDate birthday
 ) {
     // 정적 팩토리 메서드
     public static MemberInfoResDto of(String nickname, String email, Long age, String location,
-                                      String memberPictureUrl, List<String> interests, boolean profileCompleted) {
-        return new MemberInfoResDto(nickname, email, age, location, memberPictureUrl, interests, profileCompleted);
+                                      String memberPictureUrl, List<String> interests, boolean profileCompleted, LocalDate birthday) {
+        return new MemberInfoResDto(nickname, email, age, location, memberPictureUrl, interests, profileCompleted, birthday);
     }
 }
